047ef8305fed1ca21965ad971c83f9e907489b00
Shubham Dhole <shubhamdhole215@gmail.com>
2025-03-01 00:43:21 +0530

    integrated clerk auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
-
 export default Header;
