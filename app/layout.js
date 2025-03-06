3c3f4cc94ff5de8b4f295f81d1fdfd9037dcd93b
Shubham Dhole <shubhamdhole215@gmail.com>
2025-03-06 17:37:20 +0530

    adding User to DB

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/Header";
+import header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
--- components/Header.jsx
+++ components/Header.jsx
@@ -26,8 +26,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { CheckUser } from "@/lib/checkUser";
 
-const Header = () => {
+export default async function Header() {
+  await CheckUser;
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -109,5 +111,4 @@ const Header = () => {
       </nav>
     </header>
   );
-};
-export default Header;
+}
