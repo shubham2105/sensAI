d288441007d48148c12a8d8d58488fd9fab98b26
Shubham Dhole <shubhamdhole215@gmail.com>
2025-03-22 20:07:23 +0530

    fixing build related issues -1: multiple Header imports in root layout.js file

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import Header from "@/components/header";
 import { Toaster } from "sonner";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
